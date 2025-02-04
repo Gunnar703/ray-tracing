5d2495602dee06ca9a51cf85fa6f9bee91c864c7
Gunnar703 <aglorestarleaf@gmail.com>
2025-02-04 14:13:16 -0500

    add main.cu + CUDA_CHECK macro

--- rtweekend.h
+++ rtweekend.h
@@ -7,6 +7,15 @@
 #include <limits>
 #include <memory>
 
+#define CUDA_CHECK(val) check_cuda( (val), #val, __FILE__, __LINE__ )
+
+void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line) {
+    if (result) {
+        std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at " << file << ":" << line << " '" << func << "' \n";
+        cudaDeviceReset();
+        exit(99);
+    }
+}
 
 // C++ Std Usings
 
